cdb93a5cd955c61e4133942ee79d21e741b8afe2
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-24 14:06:18 +1000

    Add `/healthz` readiness endpoint

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
