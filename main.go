49b142cf332ef5703adca109da57be7b0d959d38
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-24 14:37:04 +1000

    Add `/metrics` and `/reset` endpoints as well as counter middleware

--- main.go
+++ main.go
@@ -3,7 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		fmt.Printf("middleware handler hit - hits before %d\n", cfg.fileserverHits.Load())
+		cfg.fileserverHits.Add(1)
+		fmt.Printf("middleware handler hit - hits after %d\n", cfg.fileserverHits.Load())
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) handleDisplayMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handleResetMetrics(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Metrics reset"))
+}
 
 func readinessHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -12,9 +39,14 @@ func readinessHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/",
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))),
+	)
+	mux.HandleFunc("/metrics", apiCfg.handleDisplayMetrics)
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/reset", apiCfg.handleResetMetrics)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
