88403b802d36c4b055387cf3a43aeaec8389c8dc
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-25 11:23:57 +1000

    Add ChirpResponse model

--- internal/chirp/chirp.go
+++ internal/chirp/chirp.go
@@ -1,12 +1,25 @@
 package chirp
 
-import "strings"
+import (
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 var profanities = []string{"kerfuffle", "sharbert", "fornax"}
 
 type Chirp struct {
-	Body   string `json:"body"`
-	UserID string `json:"user_id"`
+	Body   string    `json:"body"`
+	UserID uuid.UUID `json:"user_id"`
+}
+
+type ChirpResponse struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 func CleanChirp(chirp string) string {
